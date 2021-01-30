0073605588b147f4ed82b174251bc1af65c6c02e
Jonathan Farrell <jonathan.farrell11@gmail.com>
2021-01-30 23:42:00 +0000

    made battle calculations based on attack type, not pokemon type. also added some functions to streamline code, but more should be done. very clunky at the moment

--- battle/pokemonrevised.py
+++ battle/pokemonrevised.py
@@ -21,12 +21,20 @@ def delay_print(s):
         time.sleep(0.05)
 
 
+# a move class, so combat can be calculated by move type/attributes
+class Move:
+    def __init__(self, name, type, dmg):
+        self.name = name
+        self.type = type
+        self.dmg = dmg
+
+
 # create the class
 class Pokemon:
-    def __init__(self, name, types, moves, EVs, health="================"):
+    def __init__(self, name, type, moves, EVs, health="================"):
         # save variables as attributes
         self.name = name
-        self.types = types
+        self.type = type
         self.moves = moves
         self.attack = EVs['ATTACK']
         self.defense = EVs['DEFENSE']
@@ -39,13 +47,13 @@ class Pokemon:
         # print fight info
         print("-----POKEMON BATTLE-----")
         print(f"\n{self.name}")
-        print("TYPE/", self.types)
+        print("TYPE/", self.type)
         print("ATTACK/", self.attack)
         print("DEFENSE/", self.defense)
         print("\nVS")
 
         print(f"\n{pokemon2.name}")
-        print("TYPE/", pokemon2.types)
+        print("TYPE/", pokemon2.type)
         print("ATTACK/", pokemon2.attack)
         print("DEFENSE/", pokemon2.defense)
         print("\nVS")
@@ -53,30 +61,35 @@ class Pokemon:
         time.sleep(2)
 
         # considering type advantages
-        version = ["Fire", "Water", "Grass"]
-        for i, k in enumerate(version):
-            if self.types == k:
+        def calculate_damage(move, attacked_pokemon):
 
-                # both types the same
-                if pokemon2.types == k:
-                    self.defense *= 1.5
-                    pokemon2.defense *= 1.5
-                    string_1_attack = "\nIts not very effective..."
-                    string_2_attack = "\nIts not very effective..."
+            version = ["Fire", "Water", "Grass"]
 
-                # Poke2 is strong
-                if pokemon2.types == version[(i+1) % 3]:
-                    pokemon2.attack *= 1.5
-                    pokemon2.defense *= 1.5
-                    string_1_attack = "\nIt's not very effective..."
-                    string_2_attack = "\nIt's super effective!"
+            for i, k in enumerate(version):
+                if move.type == k:
 
-                # Poke1 is strong
-                if pokemon2.types == version[(i+2) % 3]:
-                    self.attack *= 1.5
-                    self.defense *= 1.5
-                    string_1_attack = "\nIt's super effective!"
-                    string_2_attack = "\nIt's not very effective..."
+                    # both types the same
+                    if attacked_pokemon.type == k:
+                        attacked_pokemon.defense *= 1.5
+                        print("\nIts not very effective...")
+
+                    # Poke2 is strong
+                    if attacked_pokemon.type == version[(i+1) % 3]:
+                        attacked_pokemon.defense *= 1.5
+                        print("\nIt's not very effective...")
+
+                    # attack is super effective
+                    if attacked_pokemon.type == version[(i+2) % 3]:
+                        attacked_pokemon.defense *= 0.75
+                        print("\nIt's super effective!")
+
+            # determine damage
+            attacked_pokemon.bars -= self.attack
+            attacked_pokemon.health = ""
+
+            # add back bars plus defense boost
+            for j in range(int(attacked_pokemon.bars + 0.1 * attacked_pokemon.defense)):
+                attacked_pokemon.health += "="
 
         # The fighting begins
         # loop to continue while a pokemon still has health
@@ -85,21 +98,20 @@ class Pokemon:
             print(f"{pokemon2.name}\t\tHEALTH\t{pokemon2.bars}\n")
 
             print(f"\nGo, {self.name}!")
+
+            def list_attacks(move):
+                print(move.name)
+
+            def attack(pokemon, move, attacked):
+                delay_print(f"{pokemon.name} used {move.name}! \n")
+                time.sleep(1)
+                calculate_damage(move, attacked)
+
             for i, x in enumerate(self.moves):
-                print(f"{i+1}.", x)
+                list_attacks(self.moves[i])
 
             index = int(input("Pick a move."))
-            delay_print(f"{self.name} used {self.moves[index-1]}!")
-            time.sleep(1)
-            delay_print(string_1_attack)
-
-            # determine damage
-            pokemon2.bars -= self.attack
-            pokemon2.health = ""
-
-            # add back bars plus defense boost
-            for j in range(int(pokemon2.bars + 0.1*pokemon2.defense)):
-                pokemon2.health += "="
+            attack(self, self.moves[index - 1], pokemon2)
 
             time.sleep(1)
             print(f"\n {self.name}\t\tHEALTH\t{self.bars}")
@@ -113,17 +125,7 @@ class Pokemon:
 
             # opposing pokemon chooses attacks at random, micro-AI opponent
             index = random.randint(1, 4)
-            delay_print(f"\n{pokemon2.name} used {pokemon2.moves[index-1]}!")
-            time.sleep(1)
-            delay_print(string_2_attack)
-
-            # determine damage
-            self.bars -= pokemon2.attack
-            self.health = ""
-
-            # add back bars plus defense boost
-            for j in range(int(self.bars + .1 * self.defense)):
-                self.health += "="
+            attack(pokemon2, pokemon2.moves[index - 1], self)
 
             time.sleep(1)
             print(f"\n{pokemon2.name}\t\tHEALTH\t{pokemon2.health}")
@@ -139,16 +141,25 @@ class Pokemon:
         delay_print(f"\nOpponent paid you ${money}.")
 
 
+Ember = Move("Ember", "Fire", 5)
+Tackle = Move("Tackle", "Normal", 2)
+Bite = Move("Bite", "Normal", 3)
+Scratch = Move("Scratch", "Normal", 2)
+Bubble = Move("Bubble", "Water", 4)
+WaterGun = Move("Water Gun", "Water", 3)
+IvyWhip = Move("Ivy Whip", "Grass", 4)
+
 if __name__ == '__main__':
     # create pokemon object
     charmander = Pokemon("Charmander", "Fire",
-                         ["Ember", "Scratch", "Tackle", "Bite"],
-                         {"ATTACK" : 4, "DEFENSE" : 2})
+                         [Ember, Tackle, Bite, Scratch],
+                         {"ATTACK": 4, "DEFENSE": 2})
     squirtle = Pokemon("Squirtle", "Water",
-                         ["Bubble", "Scratch", "Tackle", "Bite"],
+                         [Bubble, Scratch, Tackle, Bite],
                          {"ATTACK": 3, "DEFENSE": 3})
     bulbasaur = Pokemon("Bulbasaur", "Grass",
-                         ["Ivy Whip", "Scratch", "Tackle", "Bite"],
+                         [IvyWhip, Scratch, Tackle, Bite],
                          {"ATTACK": 2, "DEFENSE": 5})
 
     charmander.fight(squirtle)
+
